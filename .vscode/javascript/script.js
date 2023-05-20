809579be8dcb6d26ca8ec419e5f80913ba32ee66
Mersad13 <120870813+Mersad13@users.noreply.github.com>
2023-05-21 01:47:07 +0330

    How to work with every method

--- .vscode/javascript/script.js
+++ .vscode/javascript/script.js
@@ -1,72 +1,32 @@
-// var users = [
-//     {id:1,name:"ali",age:22},
-//     {id:2,name:"mohammad",age:23},
-//     {id:3,name:"reza",age:24}
-// ]
+// var ages = [20,18,19,25]
 
-// var isIn_user = users.some(function(user) {
-//     return user.name === "reza"
+// var isAll = ages.every(function (userAge) {
+//     return userAge > 17
 // })
 
-// console.log(isIn_user)
+// console.log(isAll)
 
-////////////////////////////////////////////////////
+////////////////////////////////////////////////
 
-// یک فروشگاه آنلاین پیاده سازی کنید
+// فرض کنید پلتفرمی مثل تلگرام ساخته اید و چند نفر در آن یک گروه ایجاد کردند
+// و قصد ایجاد تماس کنفرانسی دارند
 
-// به این شکل که محصولات فروشگاه خود را در دیتابیسی ذخیره کرده (آرایه) و آرایه ای برای سبد خرید کاربر داشته باشید
-// و از کاربر اسم محصولی را که قصد اضافه کردن به سبد خرید دارد ، دریافت کنید
+// اما شرط لازم برای ایجاد تماس کنفرانسی بالای 18 سال بودن اعضای گروه است
+// اگر تمامی اعضا بالای 18 سال نبودند، اجازه ایجاد تماس کنفرانسی را ندهید
 
-// اگر محصول درخواستی ک	اربر در انبار فروشگاه شما موجود بود، آن را به سبد خرید کاربر اضافه کنید
-// و سپس مبلغ کل سبد خرید را محاسبه کرده و به کاربر نمایش دهید
-// و در غیر این صورت به کاربر پیغام اتمام موجودی نمایش دهید
+var telegeram_group = [
+    {id:1,name:"mahasa",age:22},
+    {id:2,name:"reza",age:15},
+    {id:3,name:"sanaz",age:32},
+    {id:4,name:"mersad",age:21}
+]
 
-// :نکات قابل توجه
-// بعد از اضافه محصول به سبد خرید کاربر، سبد را در کنسول نمایش دهید
-// به طور دیفالت 3 محصول با قیمت های مختلف در سبد خرید کاربر موجود باشد
+var isAll = telegeram_group.every(function (ages) {
+    return ages.age > 18
+})
 
-// var prouduts = [
-//     {id:1,product_name:"tshirt",price:250},
-//     {id:2,product_name:"shose",price:100},
-//     {id:3,product_name:"cloths",price:300},
-//     {id:4,product_name:"shawl",price:50},
-//     {id:5,product_name:"jens",price:120},
-//     {id:6,product_name:"kanani",price:80}
-// ]
-
-// var cart = [
-//     {id:1,product_name:"tshirt",price:250},
-//     {id:2,product_name:"shose",price:100},
-//     {id:3,product_name:"cloths",price:300}
-// ]
-
-// var userProducts = prompt("Please add your product you want: ")
-
-// var userRequesr;
-
-// var isIn_produts = prouduts.some(function(prd) {
-//     if (prd.product_name === userProducts){
-//         userRequesr = prd
-//         return true
-//     }
-// })
-
-// if (isIn_produts === true) {
-//     var newPruduct = {
-//         id:userRequesr.id,
-//         product_name:userRequesr.product_name,
-//         price:userRequesr.price
-//     }
-//     cart.push(newPruduct)
-
-//     var sum = 0
-//     cart.forEach(function (sumProduct) {
-//         sum+=sumProduct.price
-//     })
-// } else {
-//     alert("That product dosen't exict")
-// }
-// console.log("All your product is = " , cart)
-// console.log("All your produts price is = " , sum)
-
-////////////////////////////////////////////////////////////
+if (isAll === true) {
+    console.log("You can call")
+} else {
+    console.log("You can not call")
+}
